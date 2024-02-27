131bace238be62da0cbe90e2defc8330cf1a167e
KhajiitLikeToSneak <luk.rdrm@gmail.com>
2024-02-27 23:10:00 +0200

    Added meal task creation form

--- app/src/main/java/com/pvp/app/ui/Router.kt
+++ app/src/main/java/com/pvp/app/ui/Router.kt
@@ -1,8 +1,12 @@
 package com.pvp.app.ui
 
 import androidx.compose.animation.AnimatedContentScope
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.CalendarMonth
+import androidx.compose.material.icons.outlined.Task
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
@@ -11,6 +15,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.pvp.app.R
 import com.pvp.app.ui.screen.calendar.CalendarScreen
+import com.pvp.app.ui.screen.task.CreateMealTaskForm
 
 @Composable
 fun Router(
@@ -19,37 +24,58 @@ fun Router(
 ) {
     run {
         // Initialize singleton beforehand, else values are not set fast enough.
-        Routes.routes
+        Route.routes
     }
 
     NavHost(
+        enterTransition = {
+            slideInHorizontally(
+                initialOffsetX = { -it },
+                animationSpec = tween(500)
+            )
+        },
+        exitTransition = {
+            slideOutHorizontally(
+                targetOffsetX = { it },
+                animationSpec = tween(500)
+            )
+        },
         modifier = modifier,
         navController = controller,
-        startDestination = Routes.Calendar.route
+        startDestination = Route.Calendar.route
     ) {
-        Routes.routes.forEach { r ->
-            composable(r.route) {
+        Route.routes.forEach { r ->
+            composable(route = r.route) {
                 r.screen(this)
             }
         }
     }
 }
 
-sealed class Routes(
+sealed class Route(
     val icon: ImageVector,
+    val iconDescription: String,
+    val resourceTitleId: Int,
     val route: String,
-    val routeNameId: Int,
     val screen: @Composable AnimatedContentScope.() -> Unit
 ) {
 
     companion object {
-        val routes = listOf(Calendar)
+        val routes = listOf(Calendar, CreateMeal)
     }
 
-    data object Calendar : Routes(
+    data object Calendar : Route(
         Icons.Outlined.CalendarMonth,
-        "calendar",
+        "Calendar page button icon",
         R.string.route_calendar,
+        "calendar",
         { CalendarScreen() }
+    )
+    object CreateMeal : Route(
+        Icons.Outlined.Task,
+        "Meal creation page button icon",
+        R.string.route_meal_CreationForm,
+        "Meal task creation",
+        screen = { CreateMealTaskForm() }
     )
 }
--- app/src/main/java/com/pvp/app/ui/screen/task/TaskScreen.kt
+++ app/src/main/java/com/pvp/app/ui/screen/task/TaskScreen.kt
@@ -2,6 +2,202 @@ package com.pvp.app.ui.screen.task
 
 import androidx.compose.runtime.Composable
 import com.pvp.app.model.Task
+import androidx.compose.foundation.layout.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.runtime.*
+import androidx.compose.material3.Text
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.ui.res.stringResource
+import com.pvp.app.R
+import androidx.compose.material3.Slider
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.rememberScrollState
+
+@Composable
+fun CreateMealTaskForm() {
+    var title by remember { mutableStateOf("") }
+    var duration by remember { mutableStateOf(0) }
+    var ingredients by remember { mutableStateOf("") }
+    var preparation by remember { mutableStateOf("") }
+    var description by remember { mutableStateOf("") }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.White)
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(10.dp)
+                .verticalScroll(rememberScrollState())
+        ) {
+            Text(
+                stringResource(R.string.meal_Title),
+                style = TextStyle(
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 17.sp,
+                    color = Color.Black
+                ),
+                modifier = Modifier
+                    .fillMaxWidth()
+            )
+
+            OutlinedTextField(
+                value = title,
+                onValueChange = { title = it },
+                singleLine = true,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 20.dp)
+                    .background(color = Color.White)
+                    .background(color = Color.White, shape = RoundedCornerShape(5.dp))
+                    .border(0.5.dp, Color.Black, shape = RoundedCornerShape(5.dp)),
+                textStyle = TextStyle(
+                    fontSize = 15.sp,
+                    color = Color.Black
+                )
+            )
+
+            Text(
+                stringResource(R.string.meal_Duration),
+                style = TextStyle(
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 17.sp,
+                    color = Color.Black
+                ),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 30.dp)
+            )
+
+            Slider(
+                value = duration.toFloat(),
+                onValueChange = { newValue -> duration = newValue.toInt() },
+                valueRange = 1f..180f,
+                steps = 180,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 20.dp)
+            )
+
+            Text(
+                text = "Duration: $duration minutes",
+                style = TextStyle(
+                    fontSize = 15.sp,
+                    color = Color.Black
+                ),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 10.dp)
+            )
+
+            Text(
+                stringResource(R.string.meal_Ingredients),
+                style = TextStyle(
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 17.sp,
+                    color = Color.Black
+                ),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 30.dp)
+            )
+
+            OutlinedTextField(
+                value = ingredients,
+                onValueChange = { ingredients = it },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 20.dp)
+                    .border(0.5.dp, Color.Black, shape = RoundedCornerShape(5.dp)),
+                textStyle = TextStyle(
+                    fontSize = 15.sp
+                )
+            )
+
+            Text(
+                stringResource(R.string.meal_Preparation),
+                style = TextStyle(
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 17.sp,
+                    color = Color.Black
+                ),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 30.dp)
+            )
+
+            OutlinedTextField(
+                value = preparation,
+                onValueChange = { preparation = it },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 20.dp)
+                    .border(0.5.dp, Color.Black, shape = RoundedCornerShape(5.dp)),
+                textStyle = TextStyle(
+                    fontSize = 15.sp
+                )
+            )
+
+            Text(
+                stringResource(R.string.meal_Description),
+                style = TextStyle(
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 17.sp,
+                    color = Color.Black
+                ),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 30.dp)
+            )
+
+            OutlinedTextField(
+                value = description,
+                onValueChange = { description = it },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 20.dp)
+                    .border(0.5.dp, Color.Black, shape = RoundedCornerShape(5.dp)),
+                textStyle = TextStyle(
+                    fontSize = 15.sp
+                )
+            )
+
+            Button(
+                onClick = {
+                    /*TODO: Functionality */
+                },
+                modifier = Modifier
+                    .width(120.dp)
+                    .height(70.dp)
+                    .align(Alignment.CenterHorizontally)
+                    .padding(top = 30.dp),
+            ) {
+                Text(
+                    "Create",
+                    style = TextStyle(
+                        fontSize = 15.sp,
+                        color = Color.White
+                    )
+                )
+            }
+        }
+    }
+}
+
+
 
 @Composable
 fun TaskBox(
