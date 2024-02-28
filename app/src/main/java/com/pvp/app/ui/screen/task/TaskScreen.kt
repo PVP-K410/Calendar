60730fafd70e5e9b186983caa15f1680727c2940
sbinalla <realmartynassutkus@gmail.com>
2024-02-28 10:36:52 +0200

    Update TaskScreen.kt

--- app/src/main/java/com/pvp/app/ui/screen/task/TaskScreen.kt
+++ app/src/main/java/com/pvp/app/ui/screen/task/TaskScreen.kt
@@ -1,8 +1,12 @@
 package com.pvp.app.ui.screen.task
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -20,6 +24,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -48,39 +53,55 @@ fun SportTaskForm() {
     var duration by remember { mutableStateOf("") }
     var startDate by remember { mutableStateOf(Date()) }
 
-    Column(
+    Box(
         modifier = Modifier
-            .padding(16.dp)
-            .fillMaxWidth()
+            .fillMaxSize()
+            .background(color = Color.White)
     ) {
-        Checkbox(
-            checked = supportsDistanceMetrics,
-            onCheckedChange = { supportsDistanceMetrics = it },
-            modifier = Modifier.padding(bottom = 16.dp)
-        )
+        Column(
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            //horizontalAlignment = Alignment.CenterHorizontally
+        ){
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.padding(bottom = 16.dp)
+            ) {
+            Checkbox(
+                checked = supportsDistanceMetrics,
+                onCheckedChange = { supportsDistanceMetrics = it },
+                modifier = Modifier.padding(bottom = 16.dp)
+            )
+            Text (
+                text = "Supports distance metrics",
+                modifier = Modifier.padding(start = 16.dp)
+            )}
 
-        TextField(
-            value = title,
-            onValueChange = { title = it },
-            label = { Text("Title") },
-            modifier = Modifier.fillMaxWidth().padding(bottom = 16.dp)
-        )
+            TextField(
+                value = title,
+                onValueChange = { title = it },
+                label = { Text("Title") },
+                modifier = Modifier.fillMaxWidth().padding(bottom = 16.dp)
+            )
 
-        TextField(
-            value = duration,
-            onValueChange = { duration = it },
-            label = { Text("Duration") },
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-            modifier = Modifier.fillMaxWidth().padding(bottom = 16.dp)
-        )
+            TextField(
+                value = duration,
+                onValueChange = { duration = it },
+                label = { Text("Duration") },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                modifier = Modifier.fillMaxWidth().padding(bottom = 16.dp)
+            )
 
-        DatePicker(
-            selectedDate = startDate,
-            modifier = Modifier.fillMaxWidth().padding(bottom = 16.dp)
-        )
+            DatePicker(
+                selectedDate = startDate,
+                modifier = Modifier.fillMaxWidth().padding(bottom = 16.dp)
+            )
 
-        Button(onClick = { /* ???? */ }) {
-            Text("Submit")
+            Button(onClick = { /* ???? */ }) {
+                Text("Submit")
+            }
         }
     }
 }
