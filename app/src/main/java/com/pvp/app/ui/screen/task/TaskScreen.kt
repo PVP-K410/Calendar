ee2f5363716e5997b69f133f8017057fd3296739
sbinalla <92679973+youreondrugs@users.noreply.github.com>
2024-02-28 21:48:42 +0200

    PVP-104 Add sport personal task creation form (#6)
    Co-authored-by: Klaidas <73783588+klaidoshka@users.noreply.github.com>

--- app/src/main/java/com/pvp/app/ui/screen/task/TaskScreen.kt
+++ app/src/main/java/com/pvp/app/ui/screen/task/TaskScreen.kt
@@ -1,7 +1,35 @@
 package com.pvp.app.ui.screen.task
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
 import com.pvp.app.model.Task
+import java.util.Calendar
+import java.util.Date
 
 @Composable
 fun TaskBox(
@@ -15,4 +43,160 @@ fun TaskScreen(
     task: Task
 ) {
 
+}
+
+@Composable
+fun SportTaskForm() {
+    var supportsDistanceMetrics by remember { mutableStateOf(false) }
+    var title by remember { mutableStateOf("") }
+    var duration by remember { mutableStateOf("") }
+    var startDate by remember { mutableStateOf(Date()) }
+    var description by remember { mutableStateOf("") }
+    var email by remember { mutableStateOf("") }
+    var activity by remember { mutableStateOf("") }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.White)
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+        ) {
+            TextField(
+                value = title,
+                onValueChange = { title = it },
+                label = { Text("Title") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 16.dp)
+            )
+
+            TextField(
+                value = activity,
+                onValueChange = { activity = it },
+                label = { Text("Activity") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 16.dp)
+            )
+
+            TextField(
+                value = description,
+                onValueChange = { description = it },
+                label = { Text("Description") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 16.dp)
+            )
+
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.padding(bottom = 16.dp)
+            ) {
+                Checkbox(
+                    checked = supportsDistanceMetrics,
+                    onCheckedChange = { supportsDistanceMetrics = it },
+                    modifier = Modifier.padding(bottom = 16.dp)
+                )
+
+                Text(
+                    text = "Supports distance metrics",
+                    modifier = Modifier.padding(start = 16.dp)
+                )
+            }
+
+            TextField(
+                value = duration,
+                onValueChange = { duration = it },
+                label = { Text("Duration") },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 16.dp)
+            )
+
+            TextField(
+                value = email,
+                onValueChange = { email = it },
+                label = { Text("Email") },
+                singleLine = true,
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 16.dp)
+            )
+
+            DatePicker(
+                selectedDate = startDate,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 16.dp)
+            )
+
+            Button(onClick = { /* TODO: Implement creation logic */ }) {
+                Text("Submit")
+            }
+        }
+    }
+}
+
+@Composable
+fun DatePicker(
+    selectedDate: Date,
+    modifier: Modifier = Modifier
+) {
+    val calendar = Calendar.getInstance()
+    calendar.time = selectedDate
+    val year = calendar.get(Calendar.YEAR)
+    val month = calendar.get(Calendar.MONTH)
+    val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+    Column(modifier) {
+        Text("Start Date")
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Text("Year:")
+
+            Spacer(modifier = Modifier.width(8.dp))
+
+            OutlinedTextField(
+                value = year.toString(),
+                onValueChange = { },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                modifier = Modifier.weight(1f)
+            )
+
+            Spacer(modifier = Modifier.width(16.dp))
+
+            Text("Month:")
+
+            Spacer(modifier = Modifier.width(8.dp))
+
+            OutlinedTextField(
+                value = (month + 1).toString(),
+                onValueChange = { },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                modifier = Modifier.weight(1f)
+            )
+
+            Spacer(modifier = Modifier.width(16.dp))
+
+            Text("Day:")
+
+            Spacer(modifier = Modifier.width(8.dp))
+
+            OutlinedTextField(
+                value = day.toString(),
+                onValueChange = { },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                modifier = Modifier.weight(1f)
+            )
+        }
+    }
 }
