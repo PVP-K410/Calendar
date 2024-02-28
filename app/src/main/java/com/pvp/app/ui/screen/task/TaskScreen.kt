fd855914d40d4c30ab411aa188c3625e22bb8d0d
Vytautas Jaras <vjaras202@gmail.com>
2024-02-28 14:36:51 +0200

    PVP-109 Code cleanup

--- app/src/main/java/com/pvp/app/ui/screen/task/TaskScreen.kt
+++ app/src/main/java/com/pvp/app/ui/screen/task/TaskScreen.kt
@@ -33,30 +33,115 @@ import java.time.Duration
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
+// Parses Duration object to a string
+// Format <HH> h <mm> min
+fun getDurationString(duration: Duration): String {
+    val hours = duration.toHours()
+    val minutes = duration.minusHours(hours).toMinutes()
+
+    return buildString {
+        if (hours > 0) append("$hours h")
+        if (minutes > 0) append("${if (hours > 0) " " else ""}$minutes min")
+    }
+}
+@Composable
+fun MealTaskBoxBody(
+    mealTask: MealTask
+){
+
+    Text(
+        "Main ingredient: " + mealTask.recipe,
+        textAlign = TextAlign.Left,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 8.dp)
+    )
+
+    if(mealTask.duration != null){
+        Text(
+            "Duration: " + getDurationString(mealTask.duration!!),
+            textAlign = TextAlign.Left,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 8.dp)
+        )
+    }
+}
+@Composable
+fun SportTaskBoxBody(
+    sportTask: SportTask
+) {
+
+    sportTask.activity?.let { activity ->
+        val activityText = buildString {
+            append(activity.title)
+            when {
+                sportTask.distance != null ->
+                    append(" for ${sportTask.distance} km")
+
+                sportTask.duration != null ->
+                    append(" for ${getDurationString(sportTask.duration!!)}")
+            }
+        }
+
+        Text(
+            text = activityText,
+            textAlign = TextAlign.Left,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 8.dp)
+        )
+    }
+}
+
+@Composable
+fun TaskBoxBody(
+    task: Task
+) {
+
+    task.description?.let { description ->
+        Text(
+            text = description,
+            textAlign = TextAlign.Left,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 8.dp)
+        )
+    }
+
+    task.duration?.let { duration ->
+        Text(
+            text = "Duration: ${getDurationString(duration)}",
+            textAlign = TextAlign.Left,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 8.dp)
+        )
+    }
+}
 @Composable
 fun TaskBox(
     task: Task
 ) {
+
+    // Later task.isCompleted should be used
     var checked by remember {
         mutableStateOf(false)
     }
+
     Card(
         shape = RectangleShape,
         modifier = Modifier
             .fillMaxWidth()
             .border(BorderStroke(1.dp, MaterialTheme.colorScheme.outline))
-
     ) {
+
         Column(
         ) {
-            val timeString = "Scheduled at " + task.scheduledAt.toLocalTime().format(
-                DateTimeFormatter.ofPattern("HH:mm"))
+
             Row(
                 modifier = Modifier.fillMaxWidth(),
-
-                //horizontalArrangement = Arrangement.Absolute.Left
             ) {
-
                 Checkbox(
                     checked = checked,
                     onCheckedChange = {checked = it},
@@ -73,107 +158,34 @@ fun TaskBox(
                     fontSize = 20.sp
                 )
             }
+
             Spacer(modifier = Modifier.height(4.dp))
+
             when(task){
                 is SportTask -> SportTaskBoxBody(task)
                 is MealTask -> MealTaskBoxBody(task)
                 else -> TaskBoxBody(task)
             }
+
+            val timeString = "Scheduled at ${task.scheduledAt
+                .toLocalTime()
+                .format(DateTimeFormatter.ofPattern("HH:mm"))}"
+
             Text(
                 timeString,
-                modifier = Modifier.padding(8.dp, 0.dp, 0.dp, 0.dp)
+                modifier = Modifier
+                    .padding(start = 8.dp)
             )
         }
     }
 }
 
 @Composable
-fun SportTaskBoxBody(
-    sportTask: SportTask
-){
-
-
-    //Divider(color = Color.Black, thickness = 1.dp)
-//    if(task.description != null){
-//
-//        Text(
-//            task.description.orEmpty(),
-//            textAlign = TextAlign.Left,
-//            modifier = Modifier
-//                .fillMaxWidth()
-//                .padding(8.dp, 0.dp, 0.dp, 0.dp)
-//        )
-//    }
-    if(sportTask.activity != null){
-        var activityText = sportTask.activity?.title + ""
-        if(sportTask.distance != null){
-            activityText += " for " + sportTask.distance.toString() + " km"
-        }else if(sportTask.duration != null){
-            activityText += " for " + getDurationString(sportTask.duration!!)
-        }
-
-        Text(
-            activityText,
-            textAlign = TextAlign.Left,
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(8.dp, 0.dp, 0.dp, 0.dp)
-        )
-    }
-}
-@Composable
-fun MealTaskBoxBody(
-    mealTask: MealTask
-){
-    Text(
-        "Main ingredient: " + mealTask.recipe,
-        textAlign = TextAlign.Left,
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(8.dp, 0.dp, 0.dp, 0.dp)
-    )
-    if(mealTask.duration != null){
-        Text(
-            "Duration: " + getDurationString(mealTask.duration!!),
-            textAlign = TextAlign.Left,
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(8.dp, 0.dp, 0.dp, 0.dp)
-        )
-    }
-}
-
-@Composable
-fun TaskBoxBody(
-    task: Task
-){
-    if(task.description != null){
-        Text(
-            task.description.toString(),
-            textAlign = TextAlign.Left,
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(8.dp, 0.dp, 0.dp, 0.dp)
-        )
-    }
-
-    if(task.duration != null){
-        Text(
-            "Duration: " + getDurationString(task.duration!!),
-            textAlign = TextAlign.Left,
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(8.dp, 0.dp, 0.dp, 0.dp)
-        )
-    }
-}
-
-@Preview
-@Composable
 fun TaskScreen(
-    //task: Task
+    task: Task
 ) {
     // Only for testing purposes, delete later
+    // To add preview, comment out task: Task from constructor.
     val mealTask = MealTask(
         description = "Prepare dinner",
         duration = Duration.ofMinutes(48),
@@ -188,7 +200,7 @@ fun TaskScreen(
     val sportTask = SportTask(
         activity = SportActivity.Running,
         description = "Run in the park",
-        distance = null,
+        distance = 5.0,
         duration = Duration.ofMinutes(65),
         id = "2",
         isCompleted = false,
@@ -206,16 +218,5 @@ fun TaskScreen(
         title = "Project Tasks",
         userEmail = "example@example.com"
     )
-    TaskBox(task)
-}
-
-// Parses Duration object to a string
-fun getDurationString(duration: Duration): String {
-    val hours = duration.toHours()
-    val minutes = duration.minusHours(hours).toMinutes()
-
-    return buildString {
-        if (hours > 0) append("$hours h")
-        if (minutes > 0) append("${if (hours > 0) " " else ""}$minutes min")
-    }
+    TaskBox(sportTask)
 }
