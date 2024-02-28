424aa41c7d16a524b88af52dcc6e151d58fedf1d
sbinalla <realmartynassutkus@gmail.com>
2024-02-28 19:09:35 +0200

    Added rest of the fields for sport task

--- app/src/main/java/com/pvp/app/ui/screen/task/TaskScreen.kt
+++ app/src/main/java/com/pvp/app/ui/screen/task/TaskScreen.kt
@@ -11,9 +11,13 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.Checkbox
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -28,9 +32,12 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.pvp.app.model.SportActivity
 import com.pvp.app.model.Task
 import java.util.Calendar
 import java.util.Date
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
 
 @Composable
 fun TaskBox(
@@ -52,6 +59,9 @@ fun SportTaskForm() {
     var title by remember { mutableStateOf("") }
     var duration by remember { mutableStateOf("") }
     var startDate by remember { mutableStateOf(Date()) }
+    var description by remember { mutableStateOf("") }
+    var email by remember { mutableStateOf("") }
+    var activity by remember { mutableStateOf("") }
 
     Box(
         modifier = Modifier
@@ -64,21 +74,6 @@ fun SportTaskForm() {
                 .fillMaxSize(),
             verticalArrangement = Arrangement.Center,
         ) {
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-                modifier = Modifier.padding(bottom = 16.dp)
-            ) {
-                Checkbox(
-                    checked = supportsDistanceMetrics,
-                    onCheckedChange = { supportsDistanceMetrics = it },
-                    modifier = Modifier.padding(bottom = 16.dp)
-                )
-                Text(
-                    text = "Supports distance metrics",
-                    modifier = Modifier.padding(start = 16.dp)
-                )
-            }
-
             TextField(
                 value = title,
                 onValueChange = { title = it },
@@ -89,10 +84,55 @@ fun SportTaskForm() {
             )
 
             TextField(
+                value = activity,
+                onValueChange = { activity = it },
+                label = { Text("Activity") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 16.dp)
+            )
+
+            TextField(
+                value = description,
+                onValueChange = { description = it },
+                label = { Text("Description") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 16.dp)
+            )
+
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.padding(bottom = 16.dp)
+            ) {
+                Checkbox(
+                    checked = supportsDistanceMetrics,
+                    onCheckedChange = { supportsDistanceMetrics = it },
+                    modifier = Modifier.padding(bottom = 16.dp)
+                )
+
+                Text(
+                    text = "Supports distance metrics",
+                    modifier = Modifier.padding(start = 16.dp)
+                )
+            }
+
+            TextField(
                 value = duration,
                 onValueChange = { duration = it },
                 label = { Text("Duration") },
                 keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 16.dp)
+            )
+
+            TextField(
+                value = email,
+                onValueChange = { email = it },
+                label = { Text("Email") },
+                singleLine = true,
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(bottom = 16.dp)
@@ -167,4 +207,10 @@ fun DatePicker(
             )
         }
     }
+}
+
+@Preview
+@Composable
+fun SimpleComposablePreview() {
+    SportTaskForm()
 }
