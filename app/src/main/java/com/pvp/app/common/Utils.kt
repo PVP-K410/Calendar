356e967841f7628a09346027f08efdf2697f475a
Vytautas Jaras <vjaras202@gmail.com>
2024-02-28 16:06:47 +0200

    PVP-109 Moved method getDurationString to common/Utils.kt

---
+++ app/src/main/java/com/pvp/app/common/Utils.kt
@@ -0,0 +1,22 @@
+package com.pvp.app.common
+
+import java.time.Duration
+
+/***
+ * Parses Duration object to a string
+ * Format <HH> h <mm> min
+ */
+fun getDurationString(duration: Duration): String {
+    val hours = duration.toHours()
+    val minutes = duration.minusHours(hours).toMinutes()
+
+    return buildString {
+        if (hours > 0) {
+            append("$hours h")
+        }
+
+        if (minutes > 0) {
+            append("${if (hours > 0) " " else ""}$minutes min")
+        }
+    }
+}
