d63e8c37a20f632a2aed03c908461fe22bd67696
Vytautas Jaras <vjaras202@gmail.com>
2024-03-03 16:08:09 +0200

    Added validation for checking whether string is a float

--- app/src/main/java/com/pvp/app/common/InputValidator.kt
+++ app/src/main/java/com/pvp/app/common/InputValidator.kt
@@ -1,9 +1,30 @@
 package com.pvp.app.common
 
 object InputValidator {
-    fun validateBlank(input: String, fieldName: String): List<String> =
-        if (input.isBlank()) listOf("$fieldName cannot be empty") else emptyList()
+    fun validateBlank(input: String, fieldName: String): List<String>{
+        return if (input.isBlank()) {
+             listOf("$fieldName cannot be empty")
+        } else {
+            emptyList()
+        }
+    }
 
-    fun validateLength(input: String, minLength: Int, fieldName: String): List<String> =
-        if (input.length < minLength) listOf("$fieldName must be at least $minLength characters long") else emptyList()
+
+    fun validateLength(input: String, minLength: Int, fieldName: String): List<String>{
+        return if (input.length < minLength) {
+            listOf("$fieldName must be at least $minLength characters long")
+        } else {
+            emptyList()
+        }
+    }
+
+    fun validateFloat(input: String, fieldName: String) : List<String>{
+        return try {
+            input.toFloat()
+            emptyList<String>()
+        } catch (e: NumberFormatException) {
+            listOf("$fieldName must be a valid number")
+        }
+    }
+
 }
