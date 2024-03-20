ee1772ad70e9dd7e6340b331dee02679ede5f82a
klaidosHka <klaidukast17@gmail.com>
2024-03-20 20:51:06 +0200

    PVP-183 Refactor sign-in/sign-up into one authentication screen Apply new design Add signing-in indicator with disabled sign-in button

--- app/src/main/java/com/pvp/app/ui/screen/layout/LayoutScreenUnauthenticated.kt
+++ app/src/main/java/com/pvp/app/ui/screen/layout/LayoutScreenUnauthenticated.kt
@@ -1,18 +1,12 @@
 package com.pvp.app.ui.screen.layout
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
-import com.pvp.app.ui.common.backgroundGradientRadial
 import com.pvp.app.ui.router.Route
 import com.pvp.app.ui.router.Router
-import com.pvp.app.ui.theme.CalendarTheme
 import kotlinx.coroutines.CoroutineScope
 
 @Composable
@@ -23,23 +17,16 @@ fun LayoutScreenUnauthenticated(
     scope: CoroutineScope
 ) {
     Surface(
-        modifier = Modifier
-            .fillMaxSize()
-            .backgroundGradientRadial()
-            .padding(8.dp)
+        modifier = Modifier.fillMaxSize()
     ) {
         Router(
             controller = controller,
             destinationStart = if (isAuthenticated && areSurveysFilled == false) {
                 Route.Survey
             } else {
-                Route.SignIn
+                Route.Authentication
             },
-            modifier = Modifier
-                .fillMaxSize()
-                .background(MaterialTheme.colorScheme.surface)
-                .padding(8.dp)
-                .backgroundGradientRadial(),
+            modifier = Modifier.fillMaxSize(),
             routes = Route.routesUnauthenticated,
             scope = scope
         )
