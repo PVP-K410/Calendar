8f75afc8df48f403cbe6fa94e5c1b99f3090f23d
Vytautas Jaras <vjaras202@gmail.com>
2024-03-21 21:25:44 +0200

    PVP-168 Added methods for aggregating active and total calories burned

--- app/src/main/java/com/pvp/app/service/HealthConnectServiceImpl.kt
+++ app/src/main/java/com/pvp/app/service/HealthConnectServiceImpl.kt
@@ -1,10 +1,11 @@
 package com.pvp.app.service
 
-import android.util.Log
 import androidx.health.connect.client.HealthConnectClient
+import androidx.health.connect.client.records.ActiveCaloriesBurnedRecord
 import androidx.health.connect.client.records.DistanceRecord
 import androidx.health.connect.client.records.Record
 import androidx.health.connect.client.records.StepsRecord
+import androidx.health.connect.client.records.TotalCaloriesBurnedRecord
 import androidx.health.connect.client.request.AggregateRequest
 import androidx.health.connect.client.request.ReadRecordsRequest
 import androidx.health.connect.client.time.TimeRangeFilter
@@ -17,34 +18,25 @@ class HealthConnectServiceImpl @Inject constructor(
     private val client: HealthConnectClient
 ) : HealthConnectService {
 
-    override suspend fun <T : Record> readActivityData(
-        record: KClass<T>,
-        start: java.time.Instant,
-        end: java.time.Instant
-    ): List<T> {
-        val request = ReadRecordsRequest(
-            recordType = record,
-            timeRangeFilter = TimeRangeFilter.between(start, end)
-        )
-
-        return client.readRecords(request).records
-    }
-
-    override suspend fun aggregateSteps(
+    override suspend fun aggregateActiveCalories(
         start: Instant,
         end: Instant
-    ): Long {
+    ): Double {
         return try {
             val response = client.aggregate(
                 AggregateRequest(
-                    metrics = setOf(StepsRecord.COUNT_TOTAL),
-                    timeRangeFilter = TimeRangeFilter.between(start, end)
+                    metrics = setOf(ActiveCaloriesBurnedRecord.ACTIVE_CALORIES_TOTAL),
+                    timeRangeFilter = TimeRangeFilter.between(
+                        start,
+                        end
+                    )
                 )
             )
 
-            response[StepsRecord.COUNT_TOTAL] ?: 0L
+            response[ActiveCaloriesBurnedRecord.ACTIVE_CALORIES_TOTAL]?.inCalories
+            0.0
         } catch (e: Exception) {
-            0L
+            0.0
         }
     }
 
@@ -65,9 +57,60 @@ class HealthConnectServiceImpl @Inject constructor(
 
             response[DistanceRecord.DISTANCE_TOTAL]?.inMeters ?: 0.0
         } catch (e: Exception) {
-            Log.e("DISTANCE", "EXCEPTION ${e.message}")
-
             0.0
         }
+    }
+
+    override suspend fun aggregateSteps(
+        start: Instant,
+        end: Instant
+    ): Long {
+        return try {
+            val response = client.aggregate(
+                AggregateRequest(
+                    metrics = setOf(StepsRecord.COUNT_TOTAL),
+                    timeRangeFilter = TimeRangeFilter.between(start, end)
+                )
+            )
+
+            response[StepsRecord.COUNT_TOTAL] ?: 0L
+        } catch (e: Exception) {
+            0L
+        }
+    }
+
+    override suspend fun aggregateTotalCalories(
+        start: Instant,
+        end: Instant
+    ): Double {
+        return try {
+            val response = client.aggregate(
+                AggregateRequest(
+                    metrics = setOf(TotalCaloriesBurnedRecord.ENERGY_TOTAL),
+                    timeRangeFilter = TimeRangeFilter.between(
+                        start,
+                        end
+                    )
+                )
+            )
+
+            response[TotalCaloriesBurnedRecord.ENERGY_TOTAL]?.inCalories
+            0.0
+        } catch (e: Exception) {
+            0.0
+        }
+    }
+
+    override suspend fun <T : Record> readActivityData(
+        record: KClass<T>,
+        start: java.time.Instant,
+        end: java.time.Instant
+    ): List<T> {
+        val request = ReadRecordsRequest(
+            recordType = record,
+            timeRangeFilter = TimeRangeFilter.between(start, end)
+        )
+
+        return client.readRecords(request).records
     }
 }
