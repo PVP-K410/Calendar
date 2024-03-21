1190d05f9ba8c222671d1e4aee3c10f6b97c7b67
Vytautas Jaras <vjaras202@gmail.com>
2024-03-22 00:07:20 +0200

    PVP-172 Added aggregationResult method that aggregates data based on metric chosen.

--- app/src/main/java/com/pvp/app/service/HealthConnectServiceImpl.kt
+++ app/src/main/java/com/pvp/app/service/HealthConnectServiceImpl.kt
@@ -1,6 +1,10 @@
 package com.pvp.app.service
 
+import android.health.connect.datatypes.units.Energy
+import android.health.connect.datatypes.units.Length
 import androidx.health.connect.client.HealthConnectClient
+import androidx.health.connect.client.aggregate.AggregateMetric
+import androidx.health.connect.client.aggregate.AggregationResult
 import androidx.health.connect.client.records.ActiveCaloriesBurnedRecord
 import androidx.health.connect.client.records.DistanceRecord
 import androidx.health.connect.client.records.Record
@@ -22,80 +26,63 @@ class HealthConnectServiceImpl @Inject constructor(
         start: Instant,
         end: Instant
     ): Double {
-        return try {
-            val response = client.aggregate(
-                AggregateRequest(
-                    metrics = setOf(ActiveCaloriesBurnedRecord.ACTIVE_CALORIES_TOTAL),
-                    timeRangeFilter = TimeRangeFilter.between(
-                        start,
-                        end
-                    )
-                )
-            )
-
-            response[ActiveCaloriesBurnedRecord.ACTIVE_CALORIES_TOTAL]?.inCalories ?: 0.0
-        } catch (e: Exception) {
-            0.0
-        }
+        return (aggregationResult(
+            metric = ActiveCaloriesBurnedRecord.ACTIVE_CALORIES_TOTAL,
+            start = start,
+            end = end
+        ) as? Energy)?.inCalories ?: 0.0
     }
 
     override suspend fun aggregateDistance(
         start: Instant,
         end: Instant
     ): Double {
-        return try {
-            val response = client.aggregate(
-                AggregateRequest(
-                    metrics = setOf(DistanceRecord.DISTANCE_TOTAL),
-                    timeRangeFilter = TimeRangeFilter.between(
-                        start,
-                        end
-                    )
-                )
-            )
-
-            response[DistanceRecord.DISTANCE_TOTAL]?.inMeters ?: 0.0
-        } catch (e: Exception) {
-            0.0
-        }
+        return (aggregationResult(
+            metric = DistanceRecord.DISTANCE_TOTAL,
+            start = start,
+            end = end
+        ) as? Length)?.inMeters ?: 0.0
     }
 
     override suspend fun aggregateSteps(
         start: Instant,
         end: Instant
     ): Long {
-        return try {
-            val response = client.aggregate(
-                AggregateRequest(
-                    metrics = setOf(StepsRecord.COUNT_TOTAL),
-                    timeRangeFilter = TimeRangeFilter.between(start, end)
-                )
-            )
-
-            response[StepsRecord.COUNT_TOTAL] ?: 0L
-        } catch (e: Exception) {
-            0L
-        }
+        return aggregationResult(
+            metric = StepsRecord.COUNT_TOTAL,
+            start = start,
+            end = end
+        ) as? Long ?: 0L
     }
 
     override suspend fun aggregateTotalCalories(
         start: Instant,
         end: Instant
     ): Double {
+        return (aggregationResult(
+            metric = TotalCaloriesBurnedRecord.ENERGY_TOTAL,
+            start = start,
+            end = end
+        ) as? Energy)?.inCalories ?: 0.0
+    }
+
+    private suspend fun aggregationResult(
+        metric: AggregateMetric<*>,
+        start: Instant,
+        end: Instant
+    ): Any? {
         return try {
-            val response = client.aggregate(
+            client.aggregate(
                 AggregateRequest(
-                    metrics = setOf(TotalCaloriesBurnedRecord.ENERGY_TOTAL),
+                    metrics = setOf(metric),
                     timeRangeFilter = TimeRangeFilter.between(
                         start,
                         end
                     )
                 )
-            )
-
-            response[TotalCaloriesBurnedRecord.ENERGY_TOTAL]?.inCalories ?: 0.0
+            )[metric]
         } catch (e: Exception) {
-            0.0
+            null
         }
     }
 
