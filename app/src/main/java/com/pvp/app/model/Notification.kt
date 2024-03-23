22b8a00d4cf65784a9ab477b9fe1ceb9d5697825
Aurimas <92021135+AurisTFG@users.noreply.github.com>
2024-03-23 20:14:28 +0200

    bring back id for notification

--- app/src/main/java/com/pvp/app/model/Notification.kt
+++ app/src/main/java/com/pvp/app/model/Notification.kt
@@ -1,6 +1,9 @@
 package com.pvp.app.model
 
+import kotlin.random.Random
+
 data class Notification(
+    val id: Int = Random.nextInt(),
     val channel: NotificationChannel,
     val title: String,
     val text: String
