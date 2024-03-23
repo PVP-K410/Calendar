3f3e6714e05171a393a99e40870afe46d950b828
Vytautas Jaras <vjaras202@gmail.com>
2024-03-23 20:13:57 +0200

    PVP-78 (#56)
    Co-authored-by: Klaidas <73783588+klaidoshka@users.noreply.github.com>

--- app/src/main/java/com/pvp/app/service/HealthConnectServiceImpl.kt
+++ app/src/main/java/com/pvp/app/service/HealthConnectServiceImpl.kt
@@ -1,9 +1,10 @@
 package com.pvp.app.service
 
-import android.health.connect.datatypes.units.Energy
-import android.health.connect.datatypes.units.Length
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.health.connect.client.HealthConnectClient
 import androidx.health.connect.client.aggregate.AggregateMetric
+import androidx.health.connect.client.aggregate.AggregationResult
 import androidx.health.connect.client.records.ActiveCaloriesBurnedRecord
 import androidx.health.connect.client.records.DistanceRecord
 import androidx.health.connect.client.records.Record
@@ -21,55 +22,64 @@ class HealthConnectServiceImpl @Inject constructor(
     private val client: HealthConnectClient
 ) : HealthConnectService {
 
+    @RequiresApi(Build.VERSION_CODES.UPSIDE_DOWN_CAKE)
     override suspend fun aggregateActiveCalories(
         start: Instant,
         end: Instant
     ): Double {
-        return (aggregationResult(
+        return result(
             metric = ActiveCaloriesBurnedRecord.ACTIVE_CALORIES_TOTAL,
             start = start,
             end = end
-        ) as? Energy)?.inCalories ?: 0.0
+        )?.let { aggregationResult ->
+            aggregationResult[ActiveCaloriesBurnedRecord.ACTIVE_CALORIES_TOTAL]?.inCalories ?: 0.0
+        } ?: 0.0
     }
 
     override suspend fun aggregateDistance(
         start: Instant,
         end: Instant
     ): Double {
-        return (aggregationResult(
+        return result(
             metric = DistanceRecord.DISTANCE_TOTAL,
             start = start,
             end = end
-        ) as? Length)?.inMeters ?: 0.0
+        )?.let { result ->
+            result[DistanceRecord.DISTANCE_TOTAL]?.inMeters ?: 0.0
+        } ?: 0.0
     }
 
     override suspend fun aggregateSteps(
         start: Instant,
         end: Instant
     ): Long {
-        return aggregationResult(
+        return result(
             metric = StepsRecord.COUNT_TOTAL,
             start = start,
             end = end
-        ) as? Long ?: 0L
+        )?.let { aggregationResult ->
+            aggregationResult[StepsRecord.COUNT_TOTAL]
+        } ?: 0L
     }
 
     override suspend fun aggregateTotalCalories(
         start: Instant,
         end: Instant
     ): Double {
-        return (aggregationResult(
+        return result(
             metric = TotalCaloriesBurnedRecord.ENERGY_TOTAL,
             start = start,
             end = end
-        ) as? Energy)?.inCalories ?: 0.0
+        )?.let { aggregationResult ->
+            aggregationResult[TotalCaloriesBurnedRecord.ENERGY_TOTAL]?.inCalories ?: 0.0
+        } ?: 0.0
     }
 
-    private suspend fun aggregationResult(
+    private suspend fun result(
         metric: AggregateMetric<*>,
         start: Instant,
         end: Instant
-    ): Any? {
+    ): AggregationResult? {
         return try {
             client.aggregate(
                 AggregateRequest(
@@ -79,7 +89,7 @@ class HealthConnectServiceImpl @Inject constructor(
                         end
                     )
                 )
-            )[metric]
+            )
         } catch (e: Exception) {
             null
         }
@@ -87,8 +97,8 @@ class HealthConnectServiceImpl @Inject constructor(
 
     override suspend fun <T : Record> readActivityData(
         record: KClass<T>,
-        start: java.time.Instant,
-        end: java.time.Instant
+        start: Instant,
+        end: Instant
     ): List<T> {
         val request = ReadRecordsRequest(
             recordType = record,
