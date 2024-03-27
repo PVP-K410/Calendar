17342c0bf2bb2b47ec3d43d1c649cdb768ccf914
Aurimas <92021135+AurisTFG@users.noreply.github.com>
2024-03-27 05:13:19 +0200

    notification permission handler

--- app/src/main/java/com/pvp/app/Activity.kt
+++ app/src/main/java/com/pvp/app/Activity.kt
@@ -1,11 +1,18 @@
 package com.pvp.app
 
+import android.app.AlertDialog
+import android.app.NotificationManager
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.core.app.NotificationManagerCompat
 import com.pvp.app.ui.router.Route
 import com.pvp.app.ui.screen.layout.LayoutScreenBootstrap
 import com.pvp.app.ui.theme.CalendarTheme
@@ -16,6 +23,10 @@ class Activity : ComponentActivity() {
 
     override fun onCreate(stateApp: Bundle?) {
         super.onCreate(stateApp)
+
+        if (!isNotificationEnabled(this)) {
+            showNotificationPermissionDialog(this)
+        }
 
         prepareRoutes()
 
@@ -37,5 +48,52 @@ class Activity : ComponentActivity() {
             Route.routesDrawer
             Route.routesUnauthenticated
         }
+    }
+
+    private fun isNotificationEnabled(context: Context): Boolean {
+        val enabled = NotificationManagerCompat
+            .from(context)
+            .areNotificationsEnabled()
+
+        if (!enabled) {
+            return false
+        }
+
+        val notificationManager =
+            context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        val channels = notificationManager.notificationChannels
+
+        for (channel in channels) {
+            if (channel.importance == NotificationManager.IMPORTANCE_NONE) {
+                return false
+            }
+        }
+
+        return true
+    }
+
+    private fun openNotificationSettingsForApp(context: Context) {
+        val intent = Intent().apply {
+            action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+            addCategory(Intent.CATEGORY_DEFAULT)
+            data = Uri.parse("package:" + context.packageName)
+        }
+
+        context.startActivity(intent)
+    }
+
+    private fun showNotificationPermissionDialog(context: Context) {
+        AlertDialog.Builder(context)
+            .setTitle("Enable Notifications")
+            .setMessage("Enable notifications to get reminders for tasks!")
+            .setPositiveButton("Go to Settings") { _, _ ->
+                openNotificationSettingsForApp(context)
+            }
+            .setNegativeButton(
+                "Cancel",
+                null
+            )
+            .show()
     }
 }
