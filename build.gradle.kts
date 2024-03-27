432e9c2cda2b02c3021cb955fad8f5a3d53b725b
Aurimas <92021135+AurisTFG@users.noreply.github.com>
2024-03-27 05:12:05 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.android.application") version "8.3.0" apply false
+    id("com.android.application") version "8.3.1" apply false
     id("com.google.dagger.hilt.android") version "2.50" apply false
     id("com.google.gms.google-services") version "4.4.1" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
@@ -8,7 +8,7 @@ plugins {
 
 buildscript {
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.21")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.21")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.50")
     }
 }
