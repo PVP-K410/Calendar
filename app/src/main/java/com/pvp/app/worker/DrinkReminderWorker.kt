ef3634e1e0cd6fc8856cbeb1c865fa3ffedae0b0
Aurimas <92021135+AurisTFG@users.noreply.github.com>
2024-04-16 21:33:48 +0300

    remove logging

--- app/src/main/java/com/pvp/app/worker/DrinkReminderWorker.kt
+++ app/src/main/java/com/pvp/app/worker/DrinkReminderWorker.kt
@@ -1,7 +1,6 @@
 package com.pvp.app.worker
 
 import android.content.Context
-import android.util.Log
 import androidx.hilt.work.HiltWorker
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
@@ -81,12 +80,7 @@ class DrinkReminderWorker @AssistedInject constructor(
         stateFlow.collect { (volume, isEnabled, userMass) ->
             cancelScheduledNotifications()
 
-            if (!isEnabled) {
-                Log.e("DrinkReminderWorker", "Cancelling notifications")
-            }
-
             if (userMass != 0 && isEnabled) {
-                Log.e("DrinkReminderWorker", "Scheduling notifications")
                 scheduleNotifications(
                     mass = userMass,
                     cupVolume = volume
