09b6a9d038779f0d1d2f720d8468aafab6b1d26c
Vytautas Jaras <vjaras202@gmail.com>
2024-04-18 14:10:38 +0300

    PVP-223 Ensured level changes after receiving experience

--- app/src/main/java/com/pvp/app/service/RewardServiceImpl.kt
+++ app/src/main/java/com/pvp/app/service/RewardServiceImpl.kt
@@ -2,6 +2,7 @@ package com.pvp.app.service
 
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.snapshots
+import com.pvp.app.api.ExperienceService
 import com.pvp.app.api.RewardService
 import com.pvp.app.api.UserService
 import com.pvp.app.model.Reward
@@ -12,7 +13,8 @@ import javax.inject.Inject
 
 class RewardServiceImpl @Inject constructor(
     private val database: FirebaseFirestore,
-    private val service: UserService
+    private val experienceService: ExperienceService,
+    private val userService: UserService
 ) : RewardService {
 
     private fun calculatePoints(streak: Int): Int {
@@ -31,10 +33,12 @@ class RewardServiceImpl @Inject constructor(
 
     override suspend fun get(): Reward {
         var streak = 0
-        val reward = service.user
+
+        val reward = userService.user
             .firstOrNull()
             ?.let { user ->
                 streak = user.streak.value
+
                 database
                     .collection(identifier)
                     .document(user.streak.value.toString())
@@ -43,24 +47,27 @@ class RewardServiceImpl @Inject constructor(
                     .firstOrNull() ?: Reward()
             } ?: Reward()
 
-        reward.points = calculatePoints(streak)
-        reward.experience = calculateExperience(streak)
+        reward.points += calculatePoints(streak)
+        reward.experience += calculateExperience(streak)
 
         return reward
     }
 
     override suspend fun rewardUser(reward: Reward) {
-        if (reward.decorationId > -1) {
+        if (reward.decorationId != null) {
             TODO("Decorations not yet implemented")
         }
 
-        service.user
+        userService.user
             .firstOrNull()
             ?.let { user ->
-                service.merge(
+                val experience = user.experience + reward.experience
+
+                userService.merge(
                     user.copy(
+                        experience = experience,
+                        level = experienceService.levelOf(experience),
                         points = user.points + reward.points,
-                        experience = user.experience + reward.experience
                     )
                 )
             }
