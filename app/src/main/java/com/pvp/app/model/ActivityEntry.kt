1ba9dc5da2586a3227f2f4d49666a2a26661fb32
Vytautas Jaras <vjaras202@gmail.com>
2024-04-24 14:17:21 +0300

    PVP-234 Code cleanup

--- app/src/main/java/com/pvp/app/model/ActivityEntry.kt
+++ app/src/main/java/com/pvp/app/model/ActivityEntry.kt
@@ -2,15 +2,10 @@
 
 package com.pvp.app.model
 
-import com.pvp.app.common.DurationSerializer
 import com.pvp.app.common.LocalDateSerializer
-import com.pvp.app.common.LocalTimeSerializer
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.json.JsonNames
-import java.time.Duration
 import java.time.LocalDate
-import java.time.LocalTime
 
 @Serializable
 data class ActivityEntry(
