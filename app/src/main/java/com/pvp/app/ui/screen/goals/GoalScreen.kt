8326dbd17af0ecc9534250690290cd724d30e38a
Vytautas Jaras <vjaras202@gmail.com>
2024-04-28 18:51:12 +0300

    PVP-235 Added goal creation and goal screen

---
+++ app/src/main/java/com/pvp/app/ui/screen/goals/GoalScreen.kt
@@ -0,0 +1,260 @@
+package com.pvp.app.ui.screen.goals
+
+import android.annotation.SuppressLint
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.Icon
+import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.pvp.app.model.Goal
+import com.pvp.app.ui.screen.layout.FloatingActionButton
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun GoalScreen(
+    model: GoalViewModel = hiltViewModel(),
+    modifier: Modifier
+) {
+    val state by model.state.collectAsStateWithLifecycle()
+    var isGoalDialogOpen by remember { mutableStateOf(false) }
+    val toggleGoalDialog = remember { { isGoalDialogOpen = !isGoalDialogOpen } }
+
+    Scaffold(
+        content = {
+            Box(
+                modifier = modifier
+                    .fillMaxSize()
+                    .padding(16.dp)
+            ) {
+                Column {
+                    val goals = state.currentGoals
+                    var filter by remember { mutableStateOf(GoalFilter.Weekly) }
+
+                    GoalTypeFilter(filter = filter) {
+                        if (it != filter) model.changeMonthly()
+                        filter = it
+                    }
+
+                    LazyColumn(modifier = Modifier.fillMaxWidth()) {
+                        if (!goals.any()) {
+                            item {
+                                Box(
+                                    contentAlignment = Alignment.Center,
+                                    modifier = Modifier.fillMaxWidth()
+                                ) {
+                                    Text(
+                                        fontStyle = FontStyle.Italic,
+                                        modifier = Modifier.padding(32.dp),
+                                        style = MaterialTheme.typography.bodyMedium,
+                                        text = "No goals have been set up yet."
+                                    )
+                                }
+                            }
+                        } else {
+                            items(goals) { goal ->
+                                GoalCard(goal = goal)
+                            }
+                        }
+                    }
+                }
+            }
+
+            GoalCreateDialog(
+                onClose = toggleGoalDialog,
+                isOpen = isGoalDialogOpen,
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(toggleGoalDialog)
+        },
+        floatingActionButtonPosition = FabPosition.End,
+    )
+}
+
+@Composable
+fun GoalCard(goal: Goal) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(4.dp)
+            .clip(RoundedCornerShape(10.dp))
+            .background(MaterialTheme.colorScheme.surfaceContainer)
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(8.dp)
+                .fillMaxWidth()
+        ) {
+            Text(
+                style = MaterialTheme.typography.titleMedium,
+                modifier = Modifier.fillMaxWidth(),
+                textAlign = TextAlign.Center,
+                text = goal.activity.title + if (goal.monthly) {
+                    " monthly"
+                } else {
+                    " weekly"
+                } + " goal"
+            )
+
+            Spacer(modifier = Modifier.padding(top = 4.dp))
+
+            Text(
+                style = MaterialTheme.typography.labelSmall,
+                fontSize = 10.sp,
+                modifier = Modifier.fillMaxWidth(),
+                textAlign = TextAlign.Center,
+                text = goal.startDate.toString() + " - " + goal.endDate.toString()
+            )
+
+            Spacer(modifier = Modifier.padding(top = 8.dp))
+
+            ProgressBar(goal = goal)
+
+            Spacer(modifier = Modifier.padding(top = 8.dp))
+
+            Row(
+                modifier = Modifier.padding(6.dp)
+            ) {
+                Icon(
+                    imageVector = goal.activity.icon,
+                    contentDescription = "Activity icon",
+                )
+
+                Text(
+                    style = MaterialTheme.typography.titleMedium,
+                    modifier = Modifier.padding(8.dp),
+                    textAlign = TextAlign.Justify,
+                    text = "Set goal: ${
+                        if (goal.steps) {
+                            "${goal.goal.toInt()} steps"
+                        } else {
+                            "${goal.goal} km"
+                        }
+                    }"
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun GoalTypeFilter(
+    filter: GoalFilter,
+    onClick: (GoalFilter) -> Unit
+) {
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        modifier = Modifier
+            .background(
+                MaterialTheme.colorScheme.surfaceContainer,
+                MaterialTheme.shapes.medium
+            )
+            .fillMaxWidth()
+            .padding(vertical = 4.dp)
+    ) {
+        GoalFilter.entries.forEach { filterNew ->
+            GoalTypeSelector(
+                filter = filterNew,
+                isSelected = filter == filterNew,
+                onClick = { onClick(filterNew) },
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxWidth()
+                    .height(40.dp)
+            )
+        }
+    }
+}
+
+@Composable
+private fun GoalTypeSelector(
+    filter: GoalFilter,
+    isSelected: Boolean,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = modifier
+            .clickable { onClick() }
+            .background(
+                if (isSelected) {
+                    MaterialTheme.colorScheme.secondaryContainer
+                } else {
+                    Color.Transparent
+                },
+                MaterialTheme.shapes.medium
+            )
+    ) {
+        Text(text = filter.displayName)
+    }
+}
+
+@Composable
+fun ProgressBar(goal: Goal) {
+    val progress by animateFloatAsState(
+        animationSpec = tween(durationMillis = 1000),
+        label = "ExperienceProgressAnimation",
+        targetValue = goal.progress.toFloat(),
+    )
+
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(32.dp)
+    ) {
+        LinearProgressIndicator(
+            color = MaterialTheme.colorScheme.tertiary.copy(alpha = 0.7f),
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(32.dp),
+            progress = { progress },
+            strokeCap = StrokeCap.Round,
+            trackColor = MaterialTheme.colorScheme.tertiary.copy(alpha = 0.4f)
+        )
+
+        Text(
+            text = "${goal.progress} / ${goal.goal}" + if (goal.steps) " steps" else " km",
+            style = MaterialTheme.typography.bodyMedium,
+        )
+    }
+}
+
+enum class GoalFilter(val displayName: String) {
+    Weekly("Weekly"),
+    Monthly("Monthly")
+}
---
+++ app/src/main/java/com/pvp/app/ui/screen/goals/GoalViewModel.kt
@@ -0,0 +1,202 @@
+package com.pvp.app.ui.screen.goals
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.pvp.app.api.Configuration
+import com.pvp.app.api.GoalService
+import com.pvp.app.api.UserService
+import com.pvp.app.model.Goal
+import com.pvp.app.model.SportActivity
+import com.pvp.app.model.User
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import java.time.DayOfWeek
+import java.time.LocalDate
+import java.time.temporal.TemporalAdjusters
+import javax.inject.Inject
+
+@OptIn(ExperimentalCoroutinesApi::class)
+@HiltViewModel
+class GoalViewModel @Inject constructor(
+    configuration: Configuration,
+    private val goalService: GoalService,
+    private val userService: UserService
+) : ViewModel(
+) {
+
+    private val _state = MutableStateFlow(GoalState())
+    val state = _state.asStateFlow()
+
+    val rangeKilometers = configuration.rangeKilometers
+
+    init {
+        viewModelScope.launch {
+            val flowUser = userService.user
+
+            val flowGoals = flowUser.flatMapLatest { user ->
+                user
+                    ?.let { goalService.get(user.email) }
+                    ?: flowOf(listOf())
+            }
+
+            flowUser
+                .combine(flowGoals) { user, goals ->
+                    user ?: return@combine _state.value
+                    val now = LocalDate.now()
+                    val weekStartDate = now.with(TemporalAdjusters.previousOrSame(DayOfWeek.MONDAY))
+                    val weekEndDate = now.with(TemporalAdjusters.nextOrSame(DayOfWeek.SUNDAY))
+                    GoalState(
+                        allGoals = goals,
+                        currentGoals = filterGoals(
+                            goals = goals,
+                            start = weekStartDate,
+                            end = weekEndDate,
+                            monthly = false
+                        ),
+                        user = user,
+                        monthStartDate = now.withDayOfMonth(1),
+                        monthEndDate = now
+                            .plusMonths(1)
+                            .withDayOfMonth(1),
+                        monthly = false,
+                        weekStartDate = weekStartDate,
+                        weekEndDate = weekEndDate
+                    )
+                }
+                .collect { state -> _state.update { state } }
+        }
+    }
+
+    fun create(
+        activity: SportActivity,
+        goal: Double,
+        monthly: Boolean,
+        steps: Boolean
+    ) {
+        val startDate = LocalDate.now()
+
+        val endDate = if (monthly) {
+            startDate.plusMonths(1)
+        } else {
+            startDate.plusDays(7)
+        }
+        viewModelScope.launch {
+            state
+                .first()
+                .let { state ->
+                    goalService.create(
+                        activity = activity,
+                        goal = goal,
+                        startDate = startDate,
+                        endDate = endDate,
+                        monthly = monthly,
+                        steps = steps,
+                        email = state.user!!.email
+                    )
+                }
+        }
+    }
+
+    fun changeMonthly() {
+        _state.update { currentState ->
+            currentState.copy(
+                monthly = !currentState.monthly
+            )
+        }
+
+        Log.e("GoalViewModel", "changeMonthly value ${state.value.monthly}")
+
+        filterGoals()
+    }
+
+    private fun filterGoals(
+        goals: List<Goal>,
+        start: LocalDate,
+        end: LocalDate,
+        monthly: Boolean
+    ): List<Goal> {
+        Log.e("GoalViewModel", "filterGoals value start: $start end $end monthly $monthly")
+
+        return goals.filter { goal ->
+            goal.startDate.isBefore(end.plusDays(1)) && goal.endDate.isAfter(start.minusDays(1))
+                    && goal.monthly == monthly
+        }
+    }
+
+    private fun filterGoals() {
+        val (start, end) = when (state.value.monthly) {
+            true -> {
+                Pair(
+                    state.value.monthStartDate,
+                    state.value.monthEndDate
+                )
+            }
+
+            false -> {
+                Pair(
+                    state.value.weekStartDate,
+                    state.value.weekEndDate
+                )
+            }
+        }
+        Log.e("GoalViewModel", "filterGoals value start: $start end $end")
+
+
+        _state.update { currentState ->
+            currentState.copy(
+                currentGoals = filterGoals(
+                    goals = currentState.allGoals,
+                    start = start,
+                    end = end,
+                    monthly = currentState.monthly
+                )
+            )
+        }
+    }
+
+    fun next() {
+        _state.update { currentState ->
+            currentState.copy(
+                monthStartDate = currentState.monthStartDate.plusMonths(1),
+                monthEndDate = currentState.monthEndDate.plusMonths(1),
+                weekStartDate = currentState.weekStartDate.minusDays(7),
+                weekEndDate = currentState.weekEndDate.minusDays(7)
+            )
+        }
+
+        filterGoals()
+    }
+
+    fun previous() {
+        _state.update { currentState ->
+            currentState.copy(
+                monthStartDate = currentState.monthStartDate.minusMonths(1),
+                monthEndDate = currentState.monthEndDate.minusMonths(1),
+                weekStartDate = currentState.weekStartDate.minusDays(7),
+                weekEndDate = currentState.weekEndDate.minusDays(7)
+            )
+        }
+
+        filterGoals()
+    }
+}
+
+data class GoalState(
+    val allGoals: List<Goal> = listOf(),
+    val currentGoals: List<Goal> = listOf(),
+    var monthly: Boolean = false,
+    var monthStartDate: LocalDate = LocalDate.now(),
+    var monthEndDate: LocalDate = LocalDate.now(),
+    val user: User? = null,
+    var weekStartDate: LocalDate = LocalDate.now(),
+    var weekEndDate: LocalDate = LocalDate.now()
+)
