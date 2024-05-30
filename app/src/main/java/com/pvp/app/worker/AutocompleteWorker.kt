0688edd0b7812ac17df21863fe7aceb13a1017f4
klaidosHka <klaidukast17@gmail.com>
2024-05-30 16:16:06 +0300

    PVP-278 Adjust AutocompleteWorker and set notification as silent, permanent

--- app/src/main/java/com/pvp/app/worker/AutocompleteWorker.kt
+++ app/src/main/java/com/pvp/app/worker/AutocompleteWorker.kt
@@ -2,6 +2,7 @@ package com.pvp.app.worker
 
 import android.content.Context
 import android.content.Intent
+import android.util.Log
 import androidx.core.content.ContextCompat
 import androidx.hilt.work.HiltWorker
 import androidx.work.CoroutineWorker
@@ -20,23 +21,34 @@ class AutocompleteWorker @AssistedInject constructor(
     context,
     workerParams
 ) {
+
+    override suspend fun doWork(): Result {
+        try {
+            Intent(
+                applicationContext,
+                AutocompleteService::class.java
+            )
+                .also { intent ->
+                    ContextCompat.startForegroundService(
+                        applicationContext,
+                        intent
+                    )
+                }
+        } catch (e: Exception) {
+            Log.e(
+                WORKER_NAME,
+                "Failed to start ${AutocompleteService::class.java.simpleName}. Retrying...",
+                e
+            )
+
+            return Result.retry()
+        }
+
+        return Result.success()
+    }
+
     companion object {
 
         const val WORKER_NAME = "AutocompleteWorker"
-    }
-
-    override suspend fun doWork(): Result {
-        Intent(
-            applicationContext,
-            AutocompleteService::class.java
-        )
-            .also { intent ->
-                ContextCompat.startForegroundService(
-                    applicationContext,
-                    intent
-                )
-            }
-
-        return Result.success()
     }
 }
