12b22b3f03e1123eb7744f3288e17969f4cf93aa
sbinalla <realmartynassutkus@gmail.com>
2024-05-30 13:02:26 +0300

    oops

--- app/src/main/java/com/pvp/app/ui/screen/goals/GoalScreen.kt
+++ app/src/main/java/com/pvp/app/ui/screen/goals/GoalScreen.kt
@@ -238,9 +238,10 @@ fun GoalCard(
                     style = MaterialTheme.typography.bodyMedium,
                     modifier = Modifier.padding(start = 6.dp),
                     textAlign = TextAlign.Left,
-                    text = "You're average " + when (goal.monthly) {
-                        true -> "monthly steps: %d".format((monthSteps / 30))
-                        false -> "weekly steps: %d".format((monthSteps / 30 * 7))
+                    text = if (goal.monthly) {
+                        localeStepsMonthly.format(monthSteps)
+                    } else {
+                        localeStepsWeekly.format(monthSteps / 30 * 7)
                     }
                 )
             }
